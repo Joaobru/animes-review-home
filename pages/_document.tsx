c10c703957d6015a880feef4b1f6510e42681145
joaobru <joaopaulobruno123@gmail.com>
2023-03-01 22:37:54 -0300

    feat: adding font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <link rel="stylesheet" href="css/fonts.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
